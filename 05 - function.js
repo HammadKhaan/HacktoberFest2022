db6ba530edd7a8346d3a31be5ef72565b8a41ea9
Hammad Saeed Khan <hammad.khan@iomechs.com>
2022-10-11 16:06:21 +0500

    feat: function for concatenation of strings added

--- 05 - function.js
+++ 05 - function.js
@@ -6,6 +6,10 @@ function normalFunction() {
   console.log("Normal Function")
 }
 
+function concatStrings(str1, str2) {
+  return str1.toString() + str2.toString();
+}
+
 arrowFunc();
 normalFunction();
 
@@ -15,3 +19,6 @@ const sum = (a, b = 10) => {
 
 console.log(sum(10));
 console.log(sum(10, 20));
+console.log(concatStrings('Hello','World'));
+console.log(concatStrings('Hello',1));
+console.log(concatStrings(2,1));
